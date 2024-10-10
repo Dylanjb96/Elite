c9ed291017555ffc30d80b0e56ed5d08ab911a62
Dylan Boyle <dylan96.boyle@gmail.com>
2024-10-10 13:57:32 +0100

    Update the file

--- booking/apps.py
+++ booking/apps.py
@@ -6,10 +6,5 @@ class BookingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'booking'
 
-from django.apps import AppConfig
 
-
-class BookingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'booking'
 
